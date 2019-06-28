3b0ec3a85adb870c765f557d0b437ef59787d569
alejandra cantu <alejandra.cantu13@gmail.com>
2019-06-28 22:00:42 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -60,7 +60,6 @@ def all_supplies_in_holidays(holiday_hash)
   #   Fourth Of July: Fireworks, BBQ
   # etc.
 holiday_hash.each do |key, values|
-  if key == :winter && key == :summer
     puts "#{key} #{values}"
   end
 end
