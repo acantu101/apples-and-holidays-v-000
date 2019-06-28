e076ef859b7564dfc8f6fdc8731f61e25c73d493
alejandra cantu <alejandra.cantu13@gmail.com>
2019-06-28 22:03:35 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -52,15 +52,9 @@ holiday_hash[:winter].values.flatten
 end
 
 def all_supplies_in_holidays(holiday_hash)
-  # iterate through holiday_hash and print items such that your readout resembles:
-  # Winter:
-  #   Christmas: Lights, Wreath
-  #   New Years: Party Hats
-  # Summer:
-  #   Fourth Of July: Fireworks, BBQ
-  # etc.
+
 holiday_hash.each do |key, values|
-    puts key.to_s.capitalize! :
+    puts key.to_s.capitalize!.join(:)
   end
 end
 
